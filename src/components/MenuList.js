695c9862f9d97935ab82af242ecc71549f23a869
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-20 16:57:01 +0300

    Adding css module to the menulist

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,19 +1,28 @@
 import React from 'react'
+import classes from './MenuList.module.css'
 
 function MenuList({menuItems}) {
 
     
   return (
-    <div>
+    <div  className={classes.wrapper}>
         {menuItems.map((menuItem) => {
-            const { id,price, name } = menuItem;
+            const { id,price, name, img } = menuItem;
             return (
-                <article key={id}>
+                
+               <article key={id} >
+                    <img src={img} alt=' meal ' className={classes.image}/>
                     <h1>{name}</h1>
                     <p>{price}</p>
 
                 </article>
+              
             )
+
+
+
+                
+                
         
         })}
         
