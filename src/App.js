34a576b0d4fb77d45a528fafb6f1ec2eb457e36c
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-20 14:25:36 +0300

    Adding menulist and categories components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 
 import './App.css';
 
+const menu = [
+  {
+    id: '1',
+    name: 'egg',
+    category: 'breakfast',
+    price: 25.99,
+    img: require('./assets/egg.jpg'),
+
+  }, 
+  {
+    id: '2',
+    name: 'fish',
+    category: 'breakfast',
+    price: 25.99,
+    img: require('./assets/fish.jpg'),
+    
+  },
+  {
+    id: '3',
+    name: 'hanel',
+    category: 'breakfast',
+    price: 25.99,
+    img: require('./assets/hanel.jpg'),
+    
+  }
+
+];
+
 function App() {
   return (
-    <div >
-      <h1> hello world</h1>
-     
-    </div>
+   <section>
+    <h1>Menu</h1>
+
+   </section>
   );
 }
 
